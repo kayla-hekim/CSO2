e16ee0acce7f1863c6a67a76e6d82b45e6e53882
Kayla Kim <rkf9wd@virginia.edu>
2026-04-13 17:52:04 -0400

    did scenario 0 #4 in lab

--- labs/lab12/securelab/attack.c
+++ labs/lab12/securelab/attack.c
@@ -1,9 +1,27 @@
 #include "lab.h"
+#include <string.h>
+#include <stdio.h>
 
 /* forward function for "attack 0" case.
    change this code to implement your attack
  */
 void forward_attack_0(struct message *message) {
+    if (message->from == 'A' && message->to == 'B') {
+        char* msg_data = (char*)message->data;
+
+        if (strcmp(msg_data, "PAY $1000 TO M") == 0) {
+            strcpy(msg_data, "PAY $10001438 TO M");
+            message->data_size = strlen("PAY $10001438 TO M\n");
+            send_message(message);
+            // printf("sent message with 10001438\n");
+            return;
+        }
+        else if (strcmp(msg_data, "PAY $0438 TO M") == 0) {
+            // printf("didn't have $1438 -> 10001438\n");
+            return;   
+        }
+    }
+    // printf("reached after big if block");
     send_message(message);
 }
 
